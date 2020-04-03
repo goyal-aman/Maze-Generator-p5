ab5846105b26844bf8dacb14e1a3b14b0a207ea1
goyal-aman <goyal.amanrocks@gmail.com>
2020-04-03 18:17:04 +0530

    added next cell, functionality

--- Cell.js
+++ Cell.js
@@ -1,18 +1,44 @@
-function Cell(i, j, cell_width) {
+// j->row
+//i->col
+
+function Cell(i, j, cell_width, color = [51, 51, 51]) {
     this.i = i * cell_width;
     this.j = j * cell_width;
     this.cell_width = cell_width;
+    this.col_index = floor(this.i / this.cell_width);
+    this.row_index = floor(this.j / this.cell_width);
+    this.visited = false;
+
+    // border variables
     this.top_border = true;
     this.right_border = true;
     this.bottom_border = true;
     this.left_border = true;
-    this.show = function () {
-        fill(255, 115, 255);
+
+    this.r = color[0];
+    this.g = color[1];
+    this.b = color[2];
+    this.SetColor = function (r, g, b) {
+        this.r = r;
+        this.g = g;
+        this.b = b;
+    }
+
+    this.Show = function () {
+
+        // cell without border
+        // if (this.visited) {
+        //     fill(255, 15, 240)
+        // } else {
+        //     fill(51);
+        // }
+        fill(this.r, this.g, this.b);
         noStroke();
         rect(this.i, this.j, cell_width, cell_width);
 
+        // borders
         stroke(0);
-        strokeWeight(1)
+        strokeWeight(2)
         if (this.top_border) {
             line(this.i, this.j, this.i + this.cell_width, this.j);
         }
@@ -26,4 +52,40 @@ function Cell(i, j, cell_width) {
             line(this.i, this.j + this.cell_width, this.i, this.j);
         }
     }
+
+    this.FindNextCell = function (all_cells) {
+        var Neighbors = [];
+
+        //left neighbor
+        if (this.i / this.cell_width > 0) {
+            var left_neighbor = all_cells[j][i - 1];
+            if (!left_neighbor.visited) {
+                Neighbors.push(left_neighbor);
+            }
+        }
+        // right neighbor
+        if (this.i / this.cell_width < all_cells[0].length - 1) {
+            var right_neighbor = all_cells[j][i + 1];
+            if (!right_neighbor.visited) {
+                Neighbors.push(right_neighbor);
+            }
+        }
+        // upper neighbor
+        if (this.j / this.cell_width > 0) {
+            var upper_neighbor = all_cells[j - 1][i];
+            if (!upper_neighbor.visited) {
+                Neighbors.push(upper_neighbor);
+            }
+        }
+        // lower neighbor
+        if ((this.j) / this.cell_width < all_cells.length - 1) {
+            var lower_neighbor = all_cells[j + 1][i];
+            if (!lower_neighbor.visited) {
+                Neighbors.push(lower_neighbor);
+            }
+        }
+        next_cell = random(Neighbors);
+        return next_cell;
+    }
+
 }
--- sketch.js
+++ sketch.js
@@ -1,11 +1,14 @@
 var rows, cols;
-var cell_width = 70;
+var cell_width = 20;
 var all_cells = [];
+var current_cell;
 
 function setup() {
 	createCanvas(700, 700);
 	rows = floor(height / cell_width);
 	cols = floor(width / cell_width);
+
+	// creating grid
 	for (var j = 0; j < rows; j++) {
 		var col = [];
 		for (var i = 0; i < cols; i++) {
@@ -14,16 +17,33 @@ function setup() {
 		}
 		all_cells.push(col)
 	}
+
+	current_cell = all_cells[rows - 1][cols - 1];
+	current_cell.visited = true;
+	current_cell.SetColor(255, 255, 255)
 }
 
 function draw() {
+	// frameRate(5);
 	background(51);
 
 	for (var j = 0; j < rows; j++) {
 		for (var i = 0; i < cols; i++) {
 			var cell = all_cells[i][j];
-			cell.show()
+			cell.Show()
 		}
 	}
 
+	next_cell = current_cell.FindNextCell(all_cells);
+
+	// if there are neighbor
+	if (next_cell) {
+		current_cell.SetColor(255, 15, 255)
+		next_cell.SetColor(255, 255, 255);
+		next_cell.visited = true;
+		current_cell = next_cell
+	}
+
+
 }
+
